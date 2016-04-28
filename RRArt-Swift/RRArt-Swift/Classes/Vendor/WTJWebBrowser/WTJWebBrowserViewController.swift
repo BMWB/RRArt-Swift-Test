1645fde5bf83ceecb638f1104f9a03588ed6ae60
wtj <1796231905@qq.com>
2016-04-28 17:46:45 +0800

    添加wkwebView代理方法

--- RRArt-Swift/RRArt-Swift/Classes/Vendor/WTJWebBrowser/WTJWebBrowserViewController.swift
+++ RRArt-Swift/RRArt-Swift/Classes/Vendor/WTJWebBrowser/WTJWebBrowserViewController.swift
@@ -15,6 +15,7 @@ class WTJWebBrowserViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         if wkWebView != nil{
             wkWebView?.frame = view.bounds
             wkWebView?.autoresizingMask = [UIViewAutoresizing.FlexibleWidth,UIViewAutoresizing.FlexibleHeight]
@@ -24,7 +25,7 @@ class WTJWebBrowserViewController: UIViewController {
             wkWebView?.scrollView.alwaysBounceVertical = true
             view.addSubview(wkWebView!)
         }
-        
+    
     }
     
     //MARK: -构造方法
@@ -35,10 +36,17 @@ class WTJWebBrowserViewController: UIViewController {
         wkWebView = WKWebView()
     }
     
+    
+    //配置信息，主要是做cookie的配置
+    class func webBrowserWithConfiguration( configuration:WKWebViewConfiguration) -> WTJWebBrowserViewController{
+        return WTJWebBrowserViewController(configuration: configuration)
+    }
+    
+    
     init(configuration:WKWebViewConfiguration) {
         super.init(nibName: nil, bundle: nil)
         wkWebView = WKWebView(frame: CGRectZero, configuration: configuration)
-       
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -62,6 +70,7 @@ class WTJWebBrowserViewController: UIViewController {
         
         let cookie = NSHTTPCookie(properties: properties)!
         let headers = NSHTTPCookie.requestHeaderFieldsWithCookies([cookie])
+        
         let mutableRequest = NSMutableURLRequest(URL: url)
         mutableRequest.setValue(headers["Cookie"], forHTTPHeaderField: "Cookie")
         [loadRequest(mutableRequest)]
@@ -75,13 +84,103 @@ class WTJWebBrowserViewController: UIViewController {
         }
         
     }
+    
+    func externalAppRequiredToOpenURL(url:NSURL) -> (Bool) {
+        let validSchemes = ["http","https"]
+        return validSchemes.contains(url.scheme)
+    }
+    
+    func launchExternalAppWithURL(url:NSURL) -> () {
+        
+    }
 }
 
 
 extension WTJWebBrowserViewController:WKUIDelegate,WKNavigationDelegate{
-    //MARK: -UIWebViewDelegate
+    //MARK: -WKNavigationDelegate
+    //开始加载
+    func webView(webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        
+    }
     
+    //加载完成
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        
+    }
     
-    //MARK: -WKUIDelegate,WKNavigationDelegate
+    //加载失败
+    func webView(webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: NSError) {
+        
+    }
+    
+    func webView(webView: WKWebView, didFailNavigation navigation: WKNavigation!, withError error: NSError) {
+        
+    }
+    
+    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+    
+        let url = navigationAction.request.URL
+    
+        if externalAppRequiredToOpenURL(url!){
+            
+            if (navigationAction.targetFrame == nil) {
+                
+                loadURL(url!)
+                decisionHandler(WKNavigationActionPolicy.Cancel)
+                return
+            }
+            
+        }else if UIApplication.sharedApplication().canOpenURL(url!) {
+            
+            launchExternalAppWithURL(url!)
+            decisionHandler(WKNavigationActionPolicy.Cancel)
+            return
+        }
+        
+        decisionHandler(WKNavigationActionPolicy.Allow)
+    }
+    
+    func webView(webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: (Bool) -> Void) {
+     
+        let customalert = UIAlertController(title: nil, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        customalert.addAction(UIAlertAction(title: "确定", style: UIAlertActionStyle.Default, handler: { (alert) in
+            completionHandler(true)
+        }))
+        
+        customalert.addAction(UIAlertAction(title: "取消", style: UIAlertActionStyle.Cancel, handler: { (alert) in
+            completionHandler(false)
+        }))
+        
+        presentViewController(customalert, animated: true) { 
+            
+        }
+    }
+    
+    func webView(webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: () -> Void) {
+        let customalert = UIAlertController(title: nil, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        customalert.addAction(UIAlertAction(title: "确定", style: UIAlertActionStyle.Cancel, handler: { (alert) in
+            completionHandler()
+        }))
+        
+        presentViewController(customalert, animated: true) {
+            
+        }
+        
+    }
+    
+    func webView(webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: (String?) -> Void) {
+        completionHandler("Client Not handler")
+    }
+    
+    //MARK: -WKUIDelegate
+    func webView(webView: WKWebView, createWebViewWithConfiguration configuration: WKWebViewConfiguration, forNavigationAction navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        
+        if (navigationAction.targetFrame != nil) {
+            webView .loadRequest(navigationAction.request)
+        }
+        return nil
+    }
     
 }
